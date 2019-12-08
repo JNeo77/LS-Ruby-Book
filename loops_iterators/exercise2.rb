f47eda58ab3be855dc020ec5f20ebdc3ec6c5a44
Jim Oursler <lifted_2000@yahoo.com>
2019-12-08 21:44:40 +0000

    Loops & Iterators Exercises

---
+++ loops_iterators/exercise2.rb
@@ -0,0 +1,7 @@
+x = ""
+while x != "STOP" do
+  puts "How are you doing today?"
+  ans = gets.chomp
+  puts "Would you like to keep going?"
+  x = gets.chomp
+end
---
+++ loops_iterators/exercise3.rb
@@ -0,0 +1,2 @@
+names = ["Jim", "Becky", "Kara", "Elliot", "Mason"]
+names.each_with_index { |name, index| puts "#{index+1}. #{name}" }
